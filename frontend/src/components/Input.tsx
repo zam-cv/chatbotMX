3e896065c502b38cdba63102de6877b0181f3843
zam-cv <a01799283@tec.mx>
2023-10-27 16:42:07 -0600

    input

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,3 +1,10 @@
 export default function Input() {
-  return <div></div>;
+  return (
+    <div className="flex justify-center items-center w-full h-full pr-5 pl-5">
+      <div className="lg:w-[50%] md:w-[80%] sm:w-[100%] w-[100%] grid grid-rows-1 grid-cols-[1fr_80px] gap-5">
+        <div className="bg-slate-300 p-3">input</div>
+        <div className="bg-slate-300 p-3">send</div>
+      </div>
+    </div>
+  );
 }
