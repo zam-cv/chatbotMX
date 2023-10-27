af1e92dc2bef565d4a4f84ca73d0a6f7267e8873
zam-cv <a01799283@tec.mx>
2023-10-27 14:22:38 -0600

    heasder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,17 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <header className="p-5 bg-pink-600 m">
+        <h1 className="text-4xl text-center text-white font-bold">
+          ChatBot MX
+        </h1>
+      </header>
+      <div className="container">
+        
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
