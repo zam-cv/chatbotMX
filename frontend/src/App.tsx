07802fe4a9e2cf2dd46b7dce91bc77e19987a85a
zam-cv <a01799283@tec.mx>
2023-10-27 14:29:20 -0600

    components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import './App.css'
+import "./App.css";
+import Input from "./components/Input";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <>
       <header className="p-5 bg-pink-600 m">
-        <h1 className="text-4xl text-center text-white font-bold">
+        <h1 className="text-3xl text-center text-white font-bold">
           ChatBot MX
         </h1>
       </header>
-      <div className="container">
-        
+      <div className="content">
+        <div className="chat">
+          <Chat />
+        </div>
+        <div className="input">
+          <Input />
+        </div>
       </div>
     </>
   );
