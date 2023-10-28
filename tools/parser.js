4e5dd70bbdaabf54310cd66a913b644829c63983
zam-cv <a01799283@tec.mx>
2023-10-27 22:21:33 -0600

    parser

---
+++ tools/parser.js
@@ -0,0 +1,37 @@
+import fs from "fs";
+
+function parseFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+
+function writeFile(path, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, "utf8", (err) => {
+      if (err) reject(err);
+      resolve();
+    })
+  })
+}
+
+(async () => {
+  var content = await parseFile("content.txt");
+
+  content = content.replace(/\r\n/g, "\n");
+  content = content.replace(/\\/g, "\\\\");
+  content = content.replace(/"/g, '\\"');
+  content = content.replace(/\n/g, "\\n");
+  content = content.replace(/\t/g, "\\t");
+  content = content.replace(/\v/g, "\\v");
+  content = content.replace(/\f/g, "\\f");
+  content = content.replace(/\r/g, "\\r");
+  content = content.replace(/\0/g, "\\0");
+  content = content.replace(/\x08/g, "\\b");
+
+
+  await writeFile("output.txt", content);
+})();
